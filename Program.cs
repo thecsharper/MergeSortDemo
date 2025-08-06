8d96dbbf633f08c23a98a78ccba867c8f5dd6f65
Simon Jones <si@thecsharper.com>
2025-08-06 08:26:38 +0100

    Added sort

--- Program.cs
+++ Program.cs
@@ -1,2 +1,52 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class MergeSort
+{
+    public static void Sort(int[] arr)
+    {
+        if (arr == null || arr.Length <= 1)
+            return;
+        MergeSortHelper(arr, 0, arr.Length - 1);
+    }
+
+    private static void MergeSortHelper(int[] arr, int left, int right)
+    {
+        if (left < right)
+        {
+            int mid = left + (right - left) / 2;
+            MergeSortHelper(arr, left, mid);
+            MergeSortHelper(arr, mid + 1, right);
+            Merge(arr, left, mid, right);
+        }
+    }
+
+    private static void Merge(int[] arr, int left, int mid, int right)
+    {
+        int[] temp = new int[right - left + 1];
+        int i = left, j = mid + 1, k = 0;
+
+        while (i <= mid && j <= right)
+        {
+            if (arr[i] <= arr[j])
+                temp[k++] = arr[i++];
+            else
+                temp[k++] = arr[j++];
+        }
+
+        while (i <= mid)
+            temp[k++] = arr[i++];
+        while (j <= right)
+            temp[k++] = arr[j++];
+
+        for (i = 0; i < temp.Length; i++)
+            arr[left + i] = temp[i];
+    }
+
+    public static void Main()
+    {
+        var arr = new int[] { 5, 7, 10, 3, 4 };
+        var solution = new MergeSort();
+        Sort(arr);
+
+        // Output: 3, 4, 5, 7, 10
+        Console.WriteLine($"Sorted array: {string.Join(", ", arr)}");
+    }
+}
